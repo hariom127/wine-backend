d975ceb9380ce4a8d07ad509e300d81273a1ae0f
hariom127 <career.it20@gmail.com>
2024-09-25 22:25:26 +0530

    change serving port

--- src/index.ts
+++ src/index.ts
@@ -24,8 +24,14 @@ const start = async () => {
     console.error(err)
   }
 
-  app.listen(3000, () => {
-    console.log('listening on port 3000!!!!!!!')
-  })
+  // const port = process.env.PORT || 3000;
+  // app.listen(port, () => {
+  //   console.log('listening on port 3000!!!!!!!')
+  // })
+
+  const port: number = Number(process.env.PORT) || 80;
+  app.listen(port, '0.0.0.0', () => {
+    console.log(`Server running on port ${port}`);
+  });
 }
 start()
